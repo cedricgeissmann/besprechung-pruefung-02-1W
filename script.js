82f95a3e2d00aebad2cff1907bfce31a1b4069d7
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-12-01 12:06:45 +0100

    4d

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function right() {
     const main = document.querySelector("#main")
     main.className = "right"
 }
+
+function left() {
+    const main = document.querySelector("#main")
+    main.className = "left"
+}
+
+function top() {
+    const main = document.querySelector("#main")
+    main.className = "top"
+}
+
+function bottom() {
+    const main = document.querySelector("#main")
+    main.className = "bottom"
+}
